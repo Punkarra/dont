a25137e687ed24a4965338962e112fd20a623455
Punkarra <farra.rubio@gmail.com>
2024-09-04 21:45:05 +0200

    Update dontorrent.py

--- dontorrent.py
+++ dontorrent.py
@@ -18,7 +18,7 @@ class dontorrent(object):
     possible categories are ('all', 'anime', 'books', 'games', 'movies', 'music', 'pictures', 'software', 'tv').
     """
 
-    url = 'https://dontorrent.email'
+    url = 'https://dontorrent.date/'
     name = 'dontorrent'
     supported_categories = {
         'all': '',
